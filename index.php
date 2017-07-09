70cca632dc618220b457552e6d3eea453dc5917f
ad0c1 <shaman_korea@mac.com>
2017-07-09 12:31:28 +0900

    testing

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+				<title>psychFunction Lab</title>
+		<meta name="robots" content="all" />
+		<meta name="generator" content="Sandvox 2.10.8" />
+		<meta name="viewport" content="width=769" />
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+		<link rel="image_src" href="http://www.sogum.com/_Media/screen-shot-2014-07-03-at_360-2.jpeg" type="image/jpeg" />
+		<link rel="apple-touch-icon" href="_Media/screen-shot-2014-07-03-at.jpeg" />
+		<link rel="canonical" href="http://www.sogum.com/" />
+		
+		<link rel="stylesheet" type="text/css" href="_Resources/ddsmoothmenu.css" />
+		<link rel="stylesheet" type="text/css" href="sandvox_CleanSheets/main.css" title="Clean Sheets" /><!--[if lt IE 6]>
+		<link rel="stylesheet" type="text/css" href="sandvox_CleanSheets/ie.css" /><![endif]-->
+		<!--
+		Photo credits for this website's design: <http://www.sogum.com/sandvox_CleanSheets/Credits.rtf>
+		Licensing for this website's design:     <http://www.sogum.com/sandvox_CleanSheets/License.rtf>
+		-->
+		
+	</head>
+	<body class="sandvox has-page-title no-sidebar no-custom-banner home-page IR" id="www_sogum_com" >
+				<div id="page-container">
+			<div id="page">
+				<div id="page-top" class="no-logo has-title has-tagline">
+					<div id="title">
+						<h1 class="title"><span class="in"><span style="font-size: 36px; font-family: Georgia;">psychFunction Lab</span></span></h1>
+						<p id="tagline"><span class="in">Department of Psychiatry <a href="http://eng.amc.seoul.kr/asan/lang/eng/main.do" target="_blank">ASAN Medical Center</a></span></p>
+					</div><!-- title -->
+					<div id="sitemenu-container">
+						<div id="sitemenu">
+							<h2 class="hidden">Site Navigation<a href="#page-content" rel="nofollow">[Skip]</a></h2>
+							<div id="sitemenu-content">
+								<ul>
+									<li class="i1 o"><a href="phpBB3/index.php" title="Board"><span class="in">Board</span></a></li>
+									<li class="i2 e"><a href="main_login.php/" title="Members"><span class="in">Members</span></a></li>
+									<li class="i3 o"><a href="enroll.php" title="Enroll"><span class="in">Enroll</span></a></li>
+									<li class="i4 e"><a href="photo/" title="Photo"><span class="in">Photo</span></a></li>
+									<li class="i5 o"><a href="publications.html" title="Publications"><span class="in">Publications</span></a></li>
+									<li class="i6 e last-item last hasSubmenu"><a href="blog/" title="Notes"><span class="in">Notes</span></a>
+										<ul>
+											<li class="i1 o last-item last"><a href="class/isc_study.html" title="ISC_study"><span class="in">ISC_study</span></a></li>
+										</ul>
+									</li>
+								</ul>
+							</div> <!-- /sitemenu-content -->
+						</div> <!-- /sitemenu -->
+					</div> <!-- sitemenu-container -->
+				</div> <!-- page-top -->
+				<div class="clear below-page-top"></div>
+				<div id="page-content" class="">
+					<div id="main">
+						<div id="main-top"></div>
+						<div id="main-content">
+							<h2 class="title"><span class="in"><br /></span></h2>
+							<div class="article">
+								<div class="article-content">
+									<div class="RichTextElement">
+										<div><p>                                                                                                                                                                          </p>
+											
+											<div class="first graphic-container wide center ImageElement">
+												<div class="graphic">
+													<div class="figure-content">
+														<!-- sandvox.ImageElement --><img src="_Media/screen-shot-2014-07-03-at_med.jpeg" alt="Screen Shot 2014-07-03 at 1.56.20 AM" width="496" height="427" />
+														<!-- /sandvox.ImageElement --></div>
+												</div>
+											</div><p><span data-img-src="_Media/image_med-3.png" data-img-src-hr="_Media/image_med_hr.png" data-alt="" data-width="515" data-height="65" style="width:515px; height:65px;" class="not-first-item">
+												<noscript><img src="_Media/image_med-3.png" alt="" width="515" height="65" /></noscript>
+											</span>                                                                                                                   </p><p><br /></p><p><span style="font-family: 'Nanum Gothic', sans-serif;"><br /></span></p>
+											
+										</div>
+									</div>
+								</div> <!-- /article-content -->
+								<div class="article-info">
+									<div class="timestamp in"><span class="in">29/08/2013</span></div>
+								</div> <!-- /article-info -->
+							</div> <!-- /article -->
+						</div> <!-- main-content -->
+						<div id="main-bottom"></div>
+					</div> <!-- main -->
+				</div> <!-- content -->
+				<div class="clear below-content"></div>
+				<div id="page-bottom">
+					<div id="page-bottom-contents">
+						<div> shaman_korea@mac.com 2014</div>
+						<div class="hidden"> <a rel="nofollow" href="#title">[Back To Top]</a></div>
+					</div>
+				</div> <!-- page-bottom -->
+			</div> <!-- container -->
+			<div id="extraDiv1"><span></span></div><div id="extraDiv2"><span></span></div><div id="extraDiv3"><span></span></div><div id="extraDiv4"><span></span></div><div id="extraDiv5"><span></span></div><div id="extraDiv6"><span></span></div>
+		</div> <!-- specific body type -->
+		<script src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.9.1.min.js"></script>
+		<script>
+		if (typeof jQuery === 'undefined') document.write('<scr'+'ipt src="_Resources/jquery-1.9.1.min.js"></scr'+'ipt>');
+		</script>
+		<script id="ddsmoothmenu" src="_Resources/ddsmoothmenu.js">
+			/***********************************************
+			* Smooth Navigational Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+			* This notice MUST stay intact for legal use
+			* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+			***********************************************/</script>
+		<script>
+		$(document).ready(function(){var e=window.devicePixelRatio&&window.devicePixelRatio>1?window.devicePixelRatio:1,t=window.innerWidth>window.innerHeight?screen.height:screen.width;$("span[data-img-src-hr]").each(function(){var n=$(this).data("width"),r=$(this).data("height"),i=$(this).data("alt"),s='<img src="',o=e>1&&n<2*t,u=0,a=$(this)[0].attributes;if(o){s+=$(this).data("img-src-hr")}else{s+=$(this).data("img-src")}s+='"';if(i)s+=' alt="'+i+'"';if(n)s+=' width="'+n+'"';if(r)s+=' height="'+r+'"';for(l=a.length;u<l;u++){var f=a[u].name;if(!f.match(/(data-width|data-height|data-alt|data-img-src)/g)){s+=" "+f+'="'+a[u].value+'"'}}s+=" />";$(this).replaceWith($(s))})})
+		ddsmoothmenu.arrowimages = {down:['downarrowclass', '_Resources/down.gif', 23], right:['rightarrowclass', '_Resources/right.gif']}
+		ddsmoothmenu.init({ mainmenuid: 'sitemenu-content',orientation:'h', classname:'ddsmoothmenu',contentsource:'markup'})
+		</script>
+		
+	</body>
+</html>
